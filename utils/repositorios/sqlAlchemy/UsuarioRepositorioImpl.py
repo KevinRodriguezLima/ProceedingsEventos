abfc1dbe3cb793e6d26819f7a2c629866d383395
DanielfQo <danielfrank12122004@gmail.com>
2024-07-31 22:55:25 -0500

    se arreglo los servicios y repositorios

--- utils/repositorios/sqlAlchemy/UsuarioRepositorioImpl.py
+++ utils/repositorios/sqlAlchemy/UsuarioRepositorioImpl.py
@@ -3,12 +3,30 @@
 
 from utils.repositorios.sqlAlchemy.conexionBd import db
 from models.entidades.Usuario import Usuario
+from werkzeug.security import check_password_hash
 
-def agregar_usuario_bd(usuario):
-    db.session.add(usuario)
-    db.session.commit()
+class UsuarioRepositorioImpl:
 
-def verificar_usuario_bd(usuario):
-    usuario_db = db.session.query(Usuario).filter_by(nombres=usuario.nombres, apellidos=usuario.apellidos, email=usuario.email).first()
-    return usuario_db is None
+    def __init__(self, usuario = None):
+        self.usuario = usuario
 
+    def agregar_usuario_bd(self):
+        db.session.add(self.usuario)
+        db.session.commit()
+
+    def verificar_usuario_bd(self):
+        usuario_db = db.session.query(Usuario).filter_by(nombres=self.usuario.nombres, 
+                                                         apellidos=self.usuario.apellidos, 
+                                                         email=self.usuario.email).first()
+        return usuario_db is None
+
+    def recuperar_usuario_by_email(self, email):
+        self.usuario = db.session.query(Usuario).filter_by(email=email).first()
+
+    def consultar_usuario(self):
+        if self.usuario is None:
+            return None
+        return self.usuario
+    
+    def verificar_contrasenia(self, contrasenia):
+        return check_password_hash(self.usuario.contrasenia, contrasenia)
